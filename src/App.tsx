a5bf1372549e6e2e2093e495f025b25cc38d7d2c
operio-dev <ftandassociates@gmail.com>
2026-07-05 23:00:09 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import WhyWeExist from "./components/WhyWeExist";
+import Principles from "./components/Principles";
+import WhatWeLookFor from "./components/WhatWeLookFor";
+import HowWeEvaluate from "./components/HowWeEvaluate";
+import ExecutionScore from "./components/ExecutionScore";
+import FounderStories from "./components/FounderStories";
+import Faq from "./components/Faq";
+import FinalCta from "./components/FinalCta";
+
+export default function App() {
+  return (
+    <div className="relative min-h-screen overflow-x-clip bg-ink">
+      <Navbar />
+      <main>
+        <Hero />
+        <WhyWeExist />
+        <Principles />
+        <WhatWeLookFor />
+        <HowWeEvaluate />
+        <ExecutionScore />
+        <FounderStories />
+        <Faq />
+        <FinalCta />
+      </main>
+    </div>
+  );
+}
