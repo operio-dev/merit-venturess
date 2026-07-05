4f8eb6fa1337bdf896fec4415bacadb1292ddb0e
operio-dev <ftandassociates@gmail.com>
2026-07-06 00:24:36 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
+import { Hero } from "./components/Hero"; // Corretto l'import con le graffe
 import WhyWeExist from "./components/WhyWeExist";
 import Principles from "./components/Principles";
 import WhatWeLookFor from "./components/WhatWeLookFor";
